b71880dd27ba522d3452f6b60ab6da287532ee54
purplexcite <webmaster@localhost>
2010-07-31 17:02:06 +0300

    fixes

--- application/views/order.php
+++ application/views/order.php
@@ -8,12 +8,12 @@
     
         echo '<table>';
     
-        echo '<tr><td><b>Товар:</b></td><td>'.$order['name'].' - '.$order['price'].' грн.</td></tr>';
+        echo '<tr><td><b>Товар:</b></td><td>'.$order['name'].' - '.$order['price'].' грн. за килограмм</td></tr>';
         echo '<tr><td><b>ФИО:</b></td><td>'.form::input('fio').'</td></tr>';
         echo '<tr><td><b>Адрес:</b></td><td>'.form::input('address').'</td></tr>';
-        echo '<tr><td><b>Телефон:</b> +38</td><td>'.form::input('phone').'</td></tr>';
+        echo '<tr><td><b>Телефон:</b> +38</td><td>'.form::input('phone').' <br><font size="1"><i>Напр.</i> 0501234567, без пробелов, скобок и тире</font></td></tr>';
         echo '<tr><td><b>EMail:</b></td><td>'.form::input('email').'</td></tr>';
-        echo '<tr><td><b>Количество:</b></td><td>'.form::input('count').'&nbsp;<select name="select"><option value="kg">килограмм</option><option value="g">грамм</option></select></td></tr>';
+        echo '<tr><td><b>Количество:</b></td><td>'.form::input('count').'&nbsp;<select name="select"><option value="kg">килограмм</option><option value="g">грамм</option></select><br><font size="1"><i>Напр.</i> 10 или 10.5, без пробелов</font></td></tr>';
         echo '<tr><td>'.Captcha::instance()->render().'</td><td valign="top">'.form::input('captcha').'</td></tr>';
         echo '<tr><td colspan="2" align="right">'.form::submit('submit', 'Отправить заказ').form::hidden('orderid', $order['id']).'</td></tr>';
     
