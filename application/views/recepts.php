0464fa0bae84bb25b7c51b8e871aba5536c0546e
purplexcite <webmaster@localhost>
2010-08-05 17:18:08 +0300

    views added

--- application/views/recepts.php
+++ application/views/recepts.php
@@ -1,1 +1,9 @@
-recepts
+<h2>Рецепты из морепродуктов</h2>
+<p>
+<ul>
+<li><?php echo html::anchor('recept1', 'Рецепт креветок в чесночном соусе') ?></li>
+<li><?php echo html::anchor('recept2', 'Как приготовить креветки. Рецепты.') ?></li>
+<li><?php echo html::anchor('recept3', 'Морской коктейль с рисом') ?></li>
+<li><?php echo html::anchor('recept4', 'Морепродукты с рисом') ?></li>
+<li><?php echo html::anchor('recept5', 'Рецепт салата из осьминогов') ?></li>
+</ul>
